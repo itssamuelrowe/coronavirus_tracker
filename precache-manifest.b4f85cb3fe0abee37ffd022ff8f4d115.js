1109ac5a7bcc9efdef48bf919d81024eb7c3886c
Samuel Rowe <samuelrowe1999@gmail.com>
2020-10-29 14:49:27 +0530

    Initial commit

---
+++ precache-manifest.b4f85cb3fe0abee37ffd022ff8f4d115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195610d1e163c7c81fb46269050bb3a5",
+    "url": "/coronavirus_tracker/index.html"
+  },
+  {
+    "revision": "27fa083cbb42a24412d5",
+    "url": "/coronavirus_tracker/static/css/main.0bc0153b.chunk.css"
+  },
+  {
+    "revision": "299c669586459e51687e",
+    "url": "/coronavirus_tracker/static/js/2.4637ce3f.chunk.js"
+  },
+  {
+    "revision": "5ba854edbe2007b917d7e6843a84f808",
+    "url": "/coronavirus_tracker/static/js/2.4637ce3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fa083cbb42a24412d5",
+    "url": "/coronavirus_tracker/static/js/main.95b47608.chunk.js"
+  },
+  {
+    "revision": "6eef8fc88152ac435ac9",
+    "url": "/coronavirus_tracker/static/js/runtime-main.86c86964.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus_tracker/static/media/COVID19.d7265326.png"
+  }
+]);
